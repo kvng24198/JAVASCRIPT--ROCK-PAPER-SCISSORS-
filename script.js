ec449cef11979dbe525699853681e153e4597b33
kvng24198 <kvngsamuel24198@gmail.com>
2025-02-04 07:34:00 -0800

    stuck at the game

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 "use strict";
-const[]
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.querySelector(".player-display");
+const computerDisplay = document.querySelector(".computer-display");
+const resultDisplay = document.querySelector(".result-display");
+const gameButton = document.querySelector("button");
+
+gameButton.addEventListener("click",function()){
+    const playerChoice = choices[Math.floor(Math.random() * choices.length)];
+    const computerChoice =choices[Math.floor(Math.random() * choices.length)];
+    return playerChoice,computerChoice
+}
